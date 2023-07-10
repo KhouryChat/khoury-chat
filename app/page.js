172d598c86a89c34503c6e419877998bbe9786b6
IvyWang152 <zhen.wang3@northeastern.edu>
2023-07-10 14:58:52 -0400

    zhen second commit

--- app/page.js
+++ app/page.js
@@ -12,8 +12,8 @@ export default function Home() {
   const [searchValue, setSearchValue] = useState('');
   // Dummy data simulating the data you might fetch from a database
   const [options, setOptions] = useState([
-    'Option 1', 'Option 2', 'Option 3', 'Option 4', 'Option 5', 
-    'Option 6', 'Option 7', 'Option 8', 'Option 9', 'Option 10'
+    {title:'Option 1'}, {title:'Option 2'}, {title:'Option 3'},{title:'Option 4'}, {title:'Option 5'},
+    {title:'Option 6'}, {title:'Option 7'},{title:'Option 8'},{title:'Option 9'}, {title:'Option 10'},
   ]);
 
   const handleSearch = (event) => {
@@ -25,67 +25,69 @@ export default function Home() {
     setSearchValue(option);
   };
 
-  const PostItem = ({title, views, likes}) => {
+  const filteredOptions = options.filter((option) => {
+    const regex = new RegExp(`(${searchValue}|\\d+)`, 'i');
+    return regex.test(option.title);
+  })
+
+  const PostItem = ({title, content, views, likes}) => {
     return (
-      <div className="bg-white rounded shadow p-4 mb-4">
+      <div className="bg-white rounded shadow p-4 mb-4 flex flex-col">
         <h3 className="text-xl font-bold">{title}</h3>
-        <p className="text-sm text-gray-500 flex items-center">
+        <h4 className="text-base font-bold">{content.split(/\s+/).slice(0, 10).join(" ") + "..."}</h4>
+        <div className="flex justify-between flex-grow text-sm text-gray-500">
+        <div className="flex items-center">
           <FontAwesomeIcon icon={faEye} className="mr-1" />
-          {views}
-        </p>
-        <p className="text-sm text-gray-500 flex items-center">
+          <p>{views}</p>
+        </div>
+        <div className="flex items-center">
           <FontAwesomeIcon icon={faHeart} className="mr-1" />
-          {likes}
-        </p>
+          <p>{likes}</p>
+        </div>
+        </div>
       </div>
     );
   };
 
   return (
-    <div className='bg-black pr-8'>
+    <><div className='bg-black pr-8'>
       <header className='py-6 flex flex-grow justify-between'>
-      <h1 className='px-8 text-3xl font-sans font-bold text-white'>Khoury Course Forum</h1>
-      <div className="space-x-4 flex justify-end">
+        <h1 className='px-8 text-3xl font-sans font-bold text-white'>Khoury Course Forum</h1>
+        <div className="space-x-4 flex justify-end">
           <button className="bg-red-500 text-white px-4 py-2 rounded">Register</button>
           <button className="bg-gray-500 text-white px-4 py-2 rounded">Login</button>
         </div>
       </header>
       <div className='flex justify-center py-32'>
-      <div className='flex flex-col w-1/2'>
-        <input
-          type="text"
-          placeholder="Search courses"
-          className="border-2 border-gray-300 w-full p-2 rounded"
-          value={searchValue}
-          onChange={handleSearch}
-        />
-        {searchValue &&
-          <div className='border-2 w-full bg-white rounded shadow-lg'>
-            {
-              options
-              .filter(
-                option => option.toLowerCase().includes(searchValue.toLowerCase())
-              )
-              .map(
-                option =>
-                <div key={option} className='border-b cursor-pointer hover:bg-gray-200'
-                  onClick={() => handleSelect(option)}>
-                  {option}
-                </div>)
-            }
+        <div className='flex flex-col w-1/2'>
+          <input
+            type="text"
+            placeholder="Search courses"
+            className="border-2 border-gray-300 w-full p-2 rounded"
+            value={searchValue}
+            onChange={handleSearch} />
+          {searchValue &&
+            <div className='border-2 w-full bg-white rounded shadow-lg'>
+              {filteredOptions.map(
+                  option => <div key={option.title} className='border-b cursor-pointer hover:bg-gray-200'
+                    onClick={() => handleSelect(option)}>
+                    {option.title}
+                  </div>)}
 
-          </div>
-            }
+            </div>}
         </div>
 
-        <div className="flex flex-col items-center">
-        <PostItem title="Post 1" views={100} likes={50} />
-        <PostItem title="Post 2" views={150} likes={80} />
-        <PostItem title="Post 3" views={80} likes={20} />
       </div>
-
-      </div>
-      </div>
+    </div><div className="flex justify-center py-8 bg-black">
+        <div className="w-1/2">
+          <PostItem title="Post 1" content="Great professor and great course. Definitely need take this one. " views={100} likes={50} />
+          <PostItem title="Post 2" content="What do you think of CS5800 by professor Pavlu Virgil? " views={150} likes={80} />
+          <PostItem title="Post 3" content="I'm currently looking for a study buddy for CS5002. Anyone wanna join? " views={80} likes={20} />
+          <PostItem title="Post 4" content="Horrible midterm!!! What do you feel about the midterm of Algorithms? " views={1000} likes={50} />
+          <PostItem title="Post 5" content="Just figure out a great study resource for 5800!" views={150} likes={80} />
+          <PostItem title="Post 6" content="Looking for teammates for a hackathon project" views={80} likes={20} />
+        </div>
+      </div></>
 
       
   )

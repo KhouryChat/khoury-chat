d445f9c11303f882bcdc98bc7df6dae4cd0285d7
neelthepatel <neelthepatel8@gmail.com>
2023-07-15 18:06:38 -0400

    Change ppostitem

--- components/PostItem/PostItem.js
+++ components/PostItem/PostItem.js
@@ -3,30 +3,25 @@ import { faEye, faHeart } from "@fortawesome/free-solid-svg-icons";
 
 const PostItem = ({ title, content, views, likes, onClick }) => {
   return (
-    title &&
-    content &&
-    views &&
-    likes && (
-      <div
-        onClick={onClick}
-        className="bg-white rounded shadow p-4 mb-4 flex flex-col cursor-pointer"
-      >
-        <h3 className="text-xl font-bold">{title}</h3>
-        <h4 className="text-base font-bold">
-          {content.split(/\s+/).slice(0, 10).join(" ")}
-        </h4>
-        <div className="flex justify-between flex-grow text-sm text-gray-500">
-          <div className="flex items-center">
-            <FontAwesomeIcon icon={faEye} className="mr-1" />
-            <p>{views}</p>
-          </div>
-          <div className="flex items-center">
-            <FontAwesomeIcon icon={faHeart} className="mr-1" />
-            <p>{likes}</p>
-          </div>
+    <div
+      onClick={onClick}
+      className="bg-white rounded shadow p-4 mb-4 flex flex-col cursor-pointer"
+    >
+      <h3 className="text-xl font-bold">{title}</h3>
+      <h4 className="text-base font-bold">
+        {content.split(/\s+/).slice(0, 10).join(" ")}
+      </h4>
+      <div className="flex justify-between flex-grow text-sm text-gray-500">
+        <div className="flex items-center">
+          <FontAwesomeIcon icon={faEye} className="mr-1" />
+          <p>{views}</p>
+        </div>
+        <div className="flex items-center">
+          <FontAwesomeIcon icon={faHeart} className="mr-1" />
+          <p>{likes}</p>
         </div>
       </div>
-    )
+    </div>
   );
 };
 

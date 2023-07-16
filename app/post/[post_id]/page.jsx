d62c4165939714ea0dd9d7423d41b4f41d88f008
neelthepatel <neelthepatel8@gmail.com>
2023-07-16 17:26:13 -0400

    Update firebase config

--- app/post/[post_id]/page.jsx
+++ app/post/[post_id]/page.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 const PostPage = ({ params }) => {
-  return <div>{params.post_id}</div>;
+  const postID = params.post_id;
+  const [timestamp, setTimestamp] = useState("");
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [replies, setReplies] = useState("");
+  const [views, setViews] = useState("");
+  const [likes, setLikes] = useState("");
+  const [dislikes, setDislikes] = useState("");
+  const [course, setCourse] = useState("");
+
+  return <div></div>;
 };
 
 export default PostPage;
---
+++ firebase.config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDMVrLQllv5xoFujCxmZ_pjc2PO-jAETIk",
+  authDomain: "course-app-c09f9.firebaseapp.com",
+  projectId: "course-app-c09f9",
+  storageBucket: "course-app-c09f9.appspot.com",
+  messagingSenderId: "857904452479",
+  appId: "1:857904452479:web:f72007d2f542784d6e404a",
+  measurementId: "G-X8TSM23P3S",
+};

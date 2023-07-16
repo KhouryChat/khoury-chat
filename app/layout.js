7301e544a52ce74146103a4e47066c534f9905b3
neelthepatel <neelthepatel8@gmail.com>
2023-07-16 18:55:19 -0400

    Fix meta issue

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,8 @@ import { AuthContextProvider } from "@/Context/AuthContext";
 export default function RootLayout({ children }) {
   return (
     <html>
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+
       <body className="">
         <AuthContextProvider>{children}</AuthContextProvider>
       </body>

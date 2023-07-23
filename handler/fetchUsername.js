871f5559dd8a8e983d375d609d16f7d3cffe7c7b
IvyWang152 <zhen.wang3@northeastern.edu>
2023-07-22 22:04:56 -0400

    fetch comments --from khourychat

--- handler/fetchUsername.js
+++ handler/fetchUsername.js
@@ -2,14 +2,15 @@ const fetchUserName = async (uid) => {
     try {
       const response = await fetch(`https://www.khourychat.com/api/users/${uid}`);
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error(`Network response was not ok: ${response.status} - ${response.statusText}`);
       }
       const data = await response.json();
-      return { [uid]: data.username };
+      //return { [uid]: data.username };
+      return data.username
     } catch (error) {
       console.error('There has been a problem with your fetch operation:', error);
       // If there's an error fetching the username, return "Anonymous mouse"
-      return { [uid]: 'Anonymous mouse' };
+      return 'Anonymous mouse';
     }
   };
   
